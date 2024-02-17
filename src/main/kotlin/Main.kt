83e2091195c5577f9736b1360565bd29b45654da
joestar02 <ramoncano@gmail.com>
2024-02-17 20:45:59 +0100

    kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,37 @@
 package org.example
 
-fun main() {
+fun main(args: Array<String>) {
     println("Hello World!")
+    println("Program argumentgs:   ${args.joinToString()}")
+    println("Program argumentgs:   ${args[0]}")
+    //en configuration edit podemos indicar los argumentos y correr programa. Para ello, tiene que tener la palabra reservada main.
+
+    //Comentario
+    /*
+    Comentario grande
+     */
+
+    print("print sin ln")
+    print(" print con escape\n")
+
+    var number: Int= 10
+    var name: String= "Jamila"
+
+    println(number)
+    println(name)
+    println("Name: ${name}  Number:${number}")
+
+    var n: Int = 10
+    var l: Long = 100L
+    var d: Double = 3.14
+    var f: Float = 3.14F
+    var b: Boolean= true
+    var s: String = "aoaoaooa"
+    var c: Char = 'a'
+
+    var other: Any= 20 //Se debe eviar el uso de Any,  usar el tipo
+    var other2: Any="Jamila2"
+
+
+
 }
